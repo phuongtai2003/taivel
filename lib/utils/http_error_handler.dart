dbe62476924787c34cf0bd9faa506781519f301a
Tai <tai.nguyenphuong12@gmail.com>
2023-03-28 00:09:32 +0700

    Some changes

--- lib/utils/http_error_handler.dart
+++ lib/utils/http_error_handler.dart
@@ -3,17 +3,32 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:sample_app/utils/show_snackbar.dart';
-void httpErrorHandler({required BuildContext context, required http.Response res, required VoidCallback onSuccess}){
-  switch(res.statusCode){
+
+void httpErrorHandler({
+  required BuildContext context,
+  required http.Response res,
+  required VoidCallback onSuccess,
+}) {
+  switch (res.statusCode) {
     case 200:
-    {
-      onSuccess();
-      break;
-    }
-    case 300:
-    {
-      showSnackBar(context: context, message: jsonDecode(res.body)['res']);
-    }
-    
+      {
+        onSuccess();
+        break;
+      }
+    case 400:
+      {
+        showSnackBar(context: context, message: jsonDecode(res.body)['msg']);
+        break;
+      }
+    case 500:
+      {
+        showSnackBar(context: context, message: jsonDecode(res.body)['error']);
+        break;
+      }
+    default:
+      {
+        showSnackBar(context: context, message: res.body);
+        break;
+      }
   }
 }
